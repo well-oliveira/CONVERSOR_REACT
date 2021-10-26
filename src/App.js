85d8c9d155890b57aa879f3234829f3b664944ba
well.oliveira <well.pagan148@gmail.com>
2021-10-26 11:27:58 -0300

    firt commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './App.css';
+import Conversor from './components/Conversor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conversor de Moedas</h1>
+
+      <div className="linha">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className="linha">
+      <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+      <div className="linha">
+      <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+      <div className="linha">
+      <Conversor moedaA="GBP" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="GBP"></Conversor>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
